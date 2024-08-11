d4f66368ef09ee083008ffff96564427176812ad
Luis Urrutia <luis@urrutia.me>
2024-08-11 10:19:32 -0400

    feat: init project

---
+++ .prettierrc.mjs
@@ -0,0 +1,31 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 100,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	quoteProps: "consistent",
+	trailingComma: "es5",
+	arrowParens: "always",
+	plugins: [
+		"prettier-plugin-astro",
+		"prettier-plugin-packagejson",
+		"prettier-plugin-tailwindcss",
+		"prettier-plugin-organize-imports",
+		"prettier-plugin-astro-organize-imports",
+	],
+	overrides: [
+		{
+			files: ["*.json", "*.md", "*.toml", "*.yml"],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
+
+// https://astro.build/config
+export default defineConfig({
+	compressHTML: true,
+	prefetch: true,
+	integrations: [tailwind()],
+	vite: {
+		css: {
+			devSourcemap: true,
+			transformer: "lightningcss",
+			lightningcss: {
+				targets: browserslistToTargets(browserslist(">= 0.25%")),
+			},
+		},
+		build: {
+			cssMinify: "lightningcss",
+		},
+	},
+});
