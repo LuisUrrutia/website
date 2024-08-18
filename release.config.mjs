fa8374f2eaf042556095aca49098f7277e3e1e8e
Luis Urrutia <luis@urrutia.me>
2024-08-18 08:03:04 -0400

    ci: only run deploy if new version is created

--- release.config.mjs
+++ release.config.mjs
@@ -7,15 +7,21 @@ export default {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		[
+			"@semantic-release/npm",
+			{
+				npmPublish: false,
+			},
+		],
+		[
 			"@semantic-release/github",
 			{
 				assets: [{ path: "/tmp/dist.tar.gz", label: "Static Site (tar.gz)" }],
 			},
 		],
 		[
-			"@semantic-release/npm",
+			"@semantic-release/exec",
 			{
-				npmPublish: false,
+				verifyReleaseCmd: 'echo "next_version=${nextRelease.version}" >> $GITHUB_OUTPUT',
 			},
 		],
 	],
