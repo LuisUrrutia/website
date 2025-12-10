186e5db8a17b963bee96c177b9fed2bed3a70c23
Luis Urrutia <luis@urrutia.me>
2025-12-10 23:03:38 +0100

    feat: add compress and use only tailwind in container

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import playformCompress from "@playform/compress";
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
 	experimental: {
 		svgo: true,
 	},
+
+	integrations: [playformCompress()],
 });
